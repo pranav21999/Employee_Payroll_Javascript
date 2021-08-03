8bfc5257861822fa32ad01752301d74029efb956
pranav21999 <pranavdani99@gmail.com>
2021-08-03 08:19:59 +0530

    Refactored:Use function to calculate Wages

--- employee_payroll.js
+++ employee_payroll.js
@@ -1,5 +1,5 @@
-/*uc1:
-Calculate employee daily wages
+/*UC3-
+Refactor:use function to calculate Wages 
 */
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -7,20 +7,21 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHrs(empCheck){
+	switch(empCheck) {
+		case IS_PART_TIME:
+			return PART_TIME_HOURS;
+			
+		case IS_FULL_TIME:
+			return FULL_TIME_HOURS;
+				
+		default:
+			return 0;
+			
+	}
+}
 let empHrs=0;
 let empCheck=Math.floor(Math.random()*10)%3;
-switch (empCheck) {
-	case IS_PART_TIME:
-		empHrs=PART_TIME_HOURS;
-		break;
-	case IS_FULL_TIME:
-		empHrs=FULL_TIME_HOURS;
-		break;	
-	default:
-		empHrs=0;
-		break;
-}
-
+empHrs = getWorkingHrs(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-
-console.log("Employee wage : " + empWage);
+console.log(empHrs +" hrs Employee wage : " + empWage);
