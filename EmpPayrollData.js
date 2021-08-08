22b07dc1c296ba75501a098912b58844000b25b0
pranav21999 <pranavdani99@gmail.com>
2021-08-08 22:46:25 +0530

    Added:Extende EmpPayroll class  with gender and StartDate

--- EmpPayrollData.js
+++ EmpPayrollData.js
@@ -2,24 +2,26 @@ class EmployeePayrollData {
   id;
   name;
   salary;
+  gender;
+  startDate;
 
-  constructor(id, name, salary) {
+  constructor(id, name, salary,gender,startData) {
     this.id = id;
     this.name = name;
     this.salary = salary;
+    this.gender = gender;
+    this.startDate = salary;
   }
 
   get name() { return this._name; }
   set name(name) { this._name = name; }
 
   toString() {
-    return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary;
+    return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary+"gender="+this.gender+"startDate="+this.startDate;
   }
 }
-const employeePayrolldata = new EmployeePayrollData(1, "pranav", 90000);
+const employeePayrolldata = new EmployeePayrollData(1, "pranav", 90000,"male","2020-05-06");
 console.log("Payroll Data: " + employeePayrolldata.toString());
 employeePayrolldata.name = "pranav1";
 console.log("After Replacement: " + employeePayrolldata.toString());
 
-const employeePayrolldata1 = new EmployeePayrollData(2, "sanket", 50000);
-console.log("Payroll Data: " + employeePayrolldata1.toString());
